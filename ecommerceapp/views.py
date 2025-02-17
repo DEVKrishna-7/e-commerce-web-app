17911f75584d5e60efaa162c1dd52d8fc5752a68
Krishna Patil <125390510+DEVKrishna-7@users.noreply.github.com>
2025-02-17 06:48:42 +0000

    update app

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):     # Define a function called index that takes a request as an argument.
+    return render(request, 'app/index.html')
